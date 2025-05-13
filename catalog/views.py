8714012910d1fd271085479557eb1b03d2a7dc62
Vitaly Shcheglov <cgfhnfr4@gmail.com>
2025-05-13 15:05:34 +0500

    modified files

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductForm
 from django.http import HttpResponse
 from .models import Product
-from django import forms
 from django.core.paginator import Paginator
 
 
