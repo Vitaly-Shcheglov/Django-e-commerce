e42b2eee8aac12574e2b9b8f5e1de279e5074bf5
Vitaly Shcheglov <cgfhnfr4@gmail.com>
2025-05-14 15:27:03 +0500

    Modifying output and processing queries

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import BlogPost
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blog_list.html'
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blog_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/blog_form.html'
+    fields = ['title', 'content', 'preview_image', 'is_published']
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blog/blog_form.html'
+    fields = ['title', 'content', 'preview_image', 'is_published']
+    success_url = reverse_lazy('blog_list')
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog_list')
