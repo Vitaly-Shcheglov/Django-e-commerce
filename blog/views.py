bc9d1d5b82c2e6c1b56e2666016780ee9a180f11
Vitaly Shcheglov <cgfhnfr4@gmail.com>
2025-05-15 08:36:36 +0500

    Creating Templates

--- blog/views.py
+++ blog/views.py
@@ -6,14 +6,15 @@ from .models import BlogPost
 class BlogListView(ListView):
     model = BlogPost
     template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
 
     def get_queryset(self):
         return BlogPost.objects.filter(is_published=True)
 
-
 class BlogDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/blog_detail.html'
+    context_object_name = 'post'
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
@@ -32,6 +33,7 @@ class BlogCreateView(CreateView):
     model = BlogPost
     template_name = 'blog/blog_form.html'
     fields = ['title', 'content', 'preview_image', 'is_published']
+    success_url = reverse_lazy('blog_list')
 
 class BlogUpdateView(UpdateView):
     model = BlogPost
