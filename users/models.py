8086ecf7b4096b1afc13adc8698eee35941d0522
Vitaly Shcheglov <hallovit@yandex.ru>
2025-06-01 14:32:47 +0500

    modified files

--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
+    username = None
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
